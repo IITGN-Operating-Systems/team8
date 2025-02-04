106a857a983995498985c9362b15cab9a4c87fda
Hit2737 <hitesh.kumar@iitgn.ac.in>
2025-02-05 04:01:24 +0530

    wrote code for ttywrite, but some error is causing headache

--- lib/ttywrite/src/main.rs
+++ lib/ttywrite/src/main.rs
@@ -16,7 +16,7 @@ use parsers::{parse_width, parse_stop_bits, parse_flow_control, parse_baud_rate}
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Write to TTY using the XMODEM protocol by default.")]
 struct Opt {
-    #[structopt(short = "i", help = "Input file (defaults to stdin if not set)", parse(from_os_str))]
+    #[structopt(short = "i", help = "Input fil  e (defaults to stdin if not set)", parse(from_os_str))]
     input: Option<PathBuf>,
 
     #[structopt(short = "b", long = "baud", parse(try_from_str = "parse_baud_rate"),
@@ -54,4 +54,40 @@ fn main() {
     let mut port = serial::open(&opt.tty_path).expect("path points to invalid TTY");
 
     // FIXME: Implement the `ttywrite` utility.
+    
+    let mut settings = port.read_settings().expect("Couldn't read Settings");
+    settings.set_baud_rate(opt.baud_rate).expect("Couldn't set baud_rate");
+    settings.set_char_size(opt.char_width);
+    settings.set_stop_bits(opt.stop_bits);
+    settings.set_flow_control(opt.flow_control);
+    port.write_settings(&settings).expect("Couldn't write Settings");
+    port.set_timeout(Duration::from_secs(opt.timeout)).expect("Invalid Timeout");
+
+
+    let mut input_data: Box<dyn io::Read> = match opt.input { // can't implement it without using Box<dyn io::Read> as both File and Stdin are different types and match should return same type
+        Some(path) => {
+            Box::new(BufReader::new(File::open(path).expect("Couldn't open file")))
+        },
+        None => {
+            Box::new(BufReader::new(io::stdin()))
+        }
+    };
+
+    if opt.raw == true {    
+        io::copy(&mut input_data, &mut port).expect("Couldn't copy data");
+    } else {
+        match Xmodem::transmit_with_progress(input_data, port, progress_fn) {
+            Ok(_) => println!("Transmission successful"),
+            Err(e) => panic!("Error: {:?}", e),
+        }
+    }
+
+    // Error is saying that the trait `std::str::FromStr` is not implemented for `serial::core::BaudRate` and all.
+    // But try_from_str is used when we have define custom functions instead of defining FromStr trait. So, It should work. 
+    // Don't know why it's not working.
+
+}
+
+fn progress_fn(progress: xmodem::Progress) {
+    println!("Progress: {:?}", progress);
 }
