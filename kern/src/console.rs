cc606cf18be09ad2b5737ad4e32fd18849e71bcd
bp0609 <patelbhavik03.bp@gmail.com>
2025-02-18 03:07:01 +0530

    Updated console.rs

--- kern/src/console.rs
+++ kern/src/console.rs
@@ -1,7 +1,10 @@
 use core::fmt;
 use pi::uart::MiniUart;
 use shim::io;
-
+use core::result::Result::Ok;
+use shim::io::Write;
+use core::option::Option;
+use core::option::Option::{Some, None};
 use crate::mutex::Mutex;
 
 /// A global singleton allowing read/write access to the console.
@@ -10,7 +13,7 @@ pub struct Console {
 }
 
 impl Console {
-    /// Creates a new instance of `Console`.
+    /// Creates a new instance of Console.
     const fn new() -> Console {
         Console { inner: None }
     }
@@ -23,7 +26,7 @@ impl Console {
         }
     }
 
-    /// Returns a mutable borrow to the inner `MiniUart`, initializing it as
+    /// Returns a mutable borrow to the inner MiniUart, initializing it as
     /// needed.
     fn inner(&mut self) -> &mut MiniUart {
         self.initialize();
@@ -35,7 +38,7 @@ impl Console {
         self.inner().read_byte()
     }
 
-    /// Writes the byte `byte` to the UART device.
+    /// Writes the byte byte to the UART device.
     pub fn write_byte(&mut self, byte: u8) {
         self.inner().write_byte(byte);
     }
@@ -49,7 +52,8 @@ impl io::Read for Console {
 
 impl io::Write for Console {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        slef.inner().write(buf);
+        self.inner().write(buf);
+        Ok(buf.len())
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -59,14 +63,15 @@ impl io::Write for Console {
 
 impl fmt::Write for Console {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.inner().write_str(s).map_err(|_| fmt::Error)
+        self.inner().write(s.as_bytes());
+        Ok(())
     }
 }
 
-/// Global `Console` singleton.
+/// Global Console singleton.
 pub static CONSOLE: Mutex<Console> = Mutex::new(Console::new());
 
-/// Internal function called by the `kprint[ln]!` macros.
+/// Internal function called by the kprint[ln]! macros.
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     #[cfg(not(test))]
@@ -82,14 +87,14 @@ pub fn _print(args: fmt::Arguments) {
     }
 }
 
-/// Like `println!`, but for kernel-space.
+/// Like println!, but for kernel-space.
 pub macro kprintln {
     () => (kprint!("\n")),
     ($fmt:expr) => (kprint!(concat!($fmt, "\n"))),
-    ($fmt:expr, $($arg:tt)*) => (kprint!(concat!($fmt, "\n"), $($arg)*))
+    ($fmt:expr, $($arg:tt)) => (kprint!(concat!($fmt, "\n"), $($arg)))
 }
 
-/// Like `print!`, but for kernel-space.
+/// Like print!, but for kernel-space.
 pub macro kprint($($arg:tt)*) {
     _print(format_args!($($arg)*))
 }
