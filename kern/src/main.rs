b8ec495988f8306e6854cf97a0545eb8b3bf2c84
Hit2737 <hitesh.kumar@iitgn.ac.in>
2025-02-18 18:04:54 +0530

    Fixing Errors!!!!

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -17,9 +17,9 @@ pub mod shell;
 
 use console::kprintln;
 use mutex::Mutex;
-use pi::uart::MiniUart;
+use pi::uart::{self, MiniUart};
 
-static UART: Mutex<Option<MiniUart>> = Mutex::new(None);
+//static UART: Mutex<Option<MiniUart>> = Mutex::new(None);
 use core::fmt::Write;
 
 // import shell
@@ -27,31 +27,34 @@ use shell::shell;
 
 fn kmain() -> ! {
     // Initialize the UART driver
-    UART.lock().replace(MiniUart::new());
-
+    // UART.lock().replace(MiniUart::new());
     kprintln!("UART echo test: type something!");
+    let mut u = uart::MiniUart::new();
 
     loop {
-        // Lock the UART to access it safely across cores
-        let mut uart = UART.lock();
+        // // Lock the UART to access it safely across cores
+        // let mut uart = UART.lock();
 
-        // Check if a byte is available to read
-        if let Some(uart) = uart.as_mut() {
-            if uart.has_byte() {
-                // Read the byte and echo it back
-                let byte = uart.read_byte();
-                if byte == b'\r' {
-                    uart.write_byte(b'\n');
-                }
-                else if byte == 127 {
-                    uart.write_byte(8);
-                    uart.write_byte(b' ');
-                    uart.write_byte(8);
-                }
-                // kprintln!("kprint byte: {}", byte);
-                uart.write_byte(byte);
-            }
-        }
+        // // Check if a byte is available to read
+        // if let Some(uart) = uart.as_mut() {
+        //     if uart.has_byte() {
+        //         // Read the byte and echo it back
+        //         let byte = uart.read_byte();
+        //         if byte == b'\r' {
+        //             uart.write_byte(b'\n');
+        //         }
+        //         else if byte == 127 {
+        //             uart.write_byte(8);
+        //             uart.write_byte(b' ');
+        //             uart.write_byte(8);
+        //         }
+        //         // kprintln!("kprint byte: {}", byte);
+        //         uart.write_byte(byte);
+        //     }
+        // }
+        let b = u.read_byte();
+        u.write_byte(b);
+        u.write_str("afdfds");
     }
 
 
