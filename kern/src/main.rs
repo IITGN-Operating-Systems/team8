783ee845946d4c3d319881e8b32ea5e96c7978b4
Hit2737 <hitesh.kumar@iitgn.ac.in>
2025-02-18 18:34:29 +0530

    Fixing Errors!!!!

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -16,54 +16,19 @@ pub mod mutex;
 pub mod shell;
 
 use console::kprintln;
-use mutex::Mutex;
-use pi::uart::{self, MiniUart};
-
-//static UART: Mutex<Option<MiniUart>> = Mutex::new(None);
+use pi::uart::MiniUart;
 use core::fmt::Write;
 
 // import shell
 use shell::shell;
 
 fn kmain() -> ! {
-    // Initialize the UART driver
-    // UART.lock().replace(MiniUart::new());
-    kprintln!("UART echo test: type something!");
+    kprintln!("Welcome to Rustberry Pi!");
     let mut u = uart::MiniUart::new();
 
     loop {
-        // // Lock the UART to access it safely across cores
-        // let mut uart = UART.lock();
-
-        // // Check if a byte is available to read
-        // if let Some(uart) = uart.as_mut() {
-        //     if uart.has_byte() {
-        //         // Read the byte and echo it back
-        //         let byte = uart.read_byte();
-        //         if byte == b'\r' {
-        //             uart.write_byte(b'\n');
-        //         }
-        //         else if byte == 127 {
-        //             uart.write_byte(8);
-        //             uart.write_byte(b' ');
-        //             uart.write_byte(8);
-        //         }
-        //         // kprintln!("kprint byte: {}", byte);
-        //         uart.write_byte(byte);
-        //     }
-        // }
         let b = u.read_byte();
         u.write_byte(b);
-        u.write_str("afdfds");
+        u.write_str("hi\n");
     }
-
-
-    // Initialize the UART driver
-    // UART.lock().replace(MiniUart::new());
-
-    // // Start the shell
-    // shell("$ ");
-
-    // loop {}
-
 }
