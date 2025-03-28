3569c5d9dcb081f3c8b81bb259702592e2169653
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-03-29 00:40:00 +0530

    kern/src/allocator/util.rs done and tested

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,7 +6,8 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    assert!(align.is_power_of_two(), "Alignment must be a power of 2");
+    addr & !(align - 1)
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -18,5 +19,8 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    assert!(align.is_power_of_two(), "Alignment must be a power of 2");
+    let aligned = (addr + align - 1) & !(align - 1);
+    assert!(aligned >= addr, "Aligning up overflowed the address");
+    aligned
 }
